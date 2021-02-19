7faace58cb42d2224b23405f1a484d9239b3094d
AdamMusa <adammusa2222@gmail.com>
2021-02-19 15:43:34 +0100

    first commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+print("Debut du programme ...")
+nom = input("Entrer votre nom : ")
+age = int(input(f"Entrer votre age {nom} : ")
+
+if age > 18:
+    print("vous etes adultes ")
+elif age < 18:
+    print("vous etes mineur")
+else:
+    print("on ne peut pas vous categoriser")
+    
+print("Fin du programme ...")
+
+
+
+
+
+#Programme Principal
+if  __name__ == '__main__':
+    pass
